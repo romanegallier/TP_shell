cadef542cb5a4b02190f7f599c69cab3659e8083
romanegarou <romanegallier@gmail.com>
2016-12-03 21:23:13 +0100

    :ku

---
+++ fichiers_fournis/petit_pg.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (){
+	int nb;
+
+	scanf ("%i", &nb);
+
+	printf ("%i\n",nb);
+
+
+return 0;
+}
---
+++ fichiers_fournis/shell2.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include "readcmd.h"
+
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main()
+{
+	while (1) {
+		struct cmdline *l;
+		int i, j;
+
+		printf("shell> ");
+		l = readcmd();
+
+		/* If input stream closed, normal termination */
+		if (!l) {
+			printf("exit\n");
+			exit(0);
+		}
+
+		if (l->err) {
+			/* Syntax error, read another command */
+			printf("error: %s\n", l->err);
+			continue;
+		}
+
+		if (l->in) printf("in: %s\n, je ne traite pas encore ce cas\n", l->in);
+		if (l->out) printf("out: %s\n, je ne traite pas encore ce cas\n", l->out);
+
+		/* Display each command of the pipe */ 
+		//ne sert a rien ici
+		for (i=0; l->seq[i]!=0; i++) {
+			char **cmd = l->seq[i];
+			printf("seq[%d]: ", i);
+			for (j=0; cmd[j]!=0; j++) {
+				printf("%s ", cmd[j]);
+			}
+			printf("\n");
+		}
+		
+		//cas d'une commande simple sans pipe:
+		
+		int pid,status;
+		switch( pid= fork()){
+			case -1: printf ("Probleme !!!!!\n"); exit (-1);
+			case 0 : {
+				
+				if (l->in) {
+					int f= open (l->in,O_RDONLY,S_IRUSR|S_IWUSR);
+					dup2(f, STDIN_FILENO);
+					close(f);
+				}
+				if (l->out){
+					
+					int f= open (l->out,O_WRONLY | O_TRUNC|O_CREAT,S_IRUSR|S_IWUSR);
+					
+					dup2(f, STDOUT_FILENO);
+					close(f);
+				}
+				execvp(l->seq[0][0],l->seq[0]);
+			}	 
+			default : 
+				if (waitpid(pid,&status,0)==-1){printf("Probleme !!!!\n");exit(-1);}
+				
+		}
+	}
+}
